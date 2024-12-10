9f900b3279e7ff3b98732a138e58089cd5b22065
HLancha <hugolanloz@gmail.com>
2024-12-10 10:57:19 +0100

    primera version

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: "./src", // Cambia la raíz del proyecto a "src"
+  build: {
+    rollupOptions:{
+        input:{
+            main: resolve(__dirname,"src/index.html")
+        },
+    },
+    outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
+  },
+  base: "./",
+});
